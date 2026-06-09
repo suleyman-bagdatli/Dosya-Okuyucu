92aa78f8798a6d060d9528299dc2e2e55e9d28c5
suleyman-bagdatli <suleymanbagdatli28@gmail.com>
2026-06-09 14:40:06 +0300

    Add files via upload

---
+++ Dosya Okuyucu.cs
@@ -0,0 +1,30 @@
+﻿namespace PROJE21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String line;
+            try
+            {
+                StreamReader sr = new StreamReader("D:\\Sample.txt");
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
